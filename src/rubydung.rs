624b94929a8306c3f9593dc904cd9f6a17b5e88c
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-12 15:42:06 +0200

    chore: todos todos todos todos

--- src/rubydung.rs
+++ src/rubydung.rs
@@ -12,7 +12,7 @@ use crate::{
         init_display, is_display_close_requested, is_key_down, is_mouse_button_down,
         update_display,
     },
-    level::{chunk, level::Level, level_renderer::LevelRenderer},
+    level::{chunk, frustum, level::Level, level_renderer::LevelRenderer},
     player::Player,
     timer::Timer,
     traits::{Drawable, Tickable},
@@ -40,7 +40,8 @@ pub fn check_error() {
 pub struct RubyDung {
     width: JInt,
     height: JInt,
-    fog_color: [f32; 4],
+    fog_color_0: [f32; 4],
+    fog_color_1: [f32; 4],
     timer: Timer,
     level: Option<Rc<RefCell<Level>>>,
     level_renderer: Option<Rc<RefCell<LevelRenderer>>>,
@@ -56,8 +57,9 @@ impl RubyDung {
         Self {
             width: 0,
             height: 0,
-            fog_color: [0.0; 4],
-            timer: Timer::new(60.0),
+            fog_color_0: [0.0; 4],
+            fog_color_1: [0.0; 4],
+            timer: Timer::new(20.0),
             level: None,
             level_renderer: None,
             player: None,
@@ -69,14 +71,21 @@ impl RubyDung {
     }
 
     pub fn init(&mut self) {
-        let col = 920330;
+        let col0 = 16710650;
+        let col1 = 920330;
         let fr = 0.5;
         let fg = 0.8;
         let fb = 1.0;
-        self.fog_color = [
-            ((col >> 16) & 0xFF) as f32 / 255.0,
-            ((col >> 8) & 0xFF) as f32 / 255.0,
-            (col & 0xFF) as f32 / 255.0,
+        self.fog_color_0 = [
+            ((col0 >> 16) & 0xFF) as f32 / 255.0,
+            ((col0 >> 8) & 0xFF) as f32 / 255.0,
+            (col0 & 0xFF) as f32 / 255.0,
+            1.0,
+        ];
+        self.fog_color_1 = [
+            ((col1 >> 16) & 0xFF) as f32 / 255.0,
+            ((col1 >> 8) & 0xFF) as f32 / 255.0,
+            (col1 & 0xFF) as f32 / 255.0,
             1.0,
         ];
         init_display(1024, 768);
@@ -103,6 +112,8 @@ impl RubyDung {
             gl::ClearDepth(1.0);
             gl::Enable(gl::DEPTH_TEST);
             gl::DepthFunc(gl::LEQUAL);
+            gl::Enable(3008); // TODO gl constant
+            gl::AlphaFunc(516, 0.5);
             gl::MatrixMode(gl::PROJECTION);
             gl::LoadIdentity();
             gl::MatrixMode(gl::MODELVIEW);
@@ -113,12 +124,14 @@ impl RubyDung {
         self.level = Some(level.clone());
         self.level_renderer = Some(LevelRenderer::new(level.clone()));
         self.player = Some(Player::new(level.clone()));
+        // TODO create particle engine here
 
         grab_mouse();
 
         for _i in 0..100 {
-            self.zombies
-                .push(Zombie::new(level.clone(), 128.0, 0.0, 128.0));
+            let mut zombie = Zombie::new(level.clone(), 128.0, 0.0, 128.0);
+            zombie.reset_pos();
+            self.zombies.push(zombie);
         }
     }
 
@@ -156,10 +169,11 @@ impl RubyDung {
     }
 
     pub fn tick(&mut self) {
-        for z in &mut self.zombies {
-            z.tick();
-        }
+        // TODO handle keyboard events here
+        // TODO tick level and particle engine
 
+        self.zombies.iter_mut().for_each(|z| z.tick());
+        self.zombies.retain(|z| !z.removed);
         self.player.as_mut().unwrap().tick();
     }
 
@@ -224,6 +238,7 @@ impl RubyDung {
             gl::RenderMode(gl::SELECT);
         }
         self.setup_pick_camera(a, (self.width / 2) as f32, (self.height / 2) as f32);
+        // TODO pass frustum
         self.level_renderer
             .as_mut()
             .unwrap()
@@ -277,6 +292,7 @@ impl RubyDung {
         if is_mouse_button_down(1)
             && let Some(ref hit) = hito
         {
+            // TODO use tile.destroy here
             self.level
                 .as_mut()
                 .unwrap()
@@ -314,6 +330,7 @@ impl RubyDung {
                 x += 1;
             }
 
+            // TODO use paint texture here
             self.level
                 .as_mut()
                 .unwrap()
@@ -321,51 +338,76 @@ impl RubyDung {
                 .set_tile(x, y, z, 1);
         }
 
-        if is_key_down(glfw::Key::Enter)
-            && let Err(err) = self.level.as_ref().unwrap().borrow().save()
-        {
-            eprintln!("failed to save level: {:?}", err);
-        }
-
         unsafe {
             gl::Clear(16640);
             self.setup_camera(a);
             gl::Enable(gl::CULL_FACE);
-            gl::Enable(gl::FOG);
-            gl::Fogi(gl::FOG_MODE, 2048);
-            gl::Fogf(gl::FOG_DENSITY, 0.2);
-            gl::Fogfv(gl::FOG_COLOR, self.fog_color.as_mut_ptr() as *mut _);
-            gl::Disable(gl::FOG);
+            let frustum = frustum::get_frustum();
+            // TODO update_dirty_chunks here
+            self.setup_fog(0);
+            gl::Enable(2912);
             self.level_renderer
                 .as_mut()
                 .unwrap()
                 .borrow_mut()
                 .render(self.player.as_ref().unwrap(), 0);
 
-            for z in &mut self.zombies {
-                z.render(a);
-            }
-
-            gl::Enable(gl::FOG);
+            // TODO render with particle engine
+            self.setup_fog(1);
             self.level_renderer
                 .as_mut()
                 .unwrap()
                 .borrow_mut()
                 .render(self.player.as_ref().unwrap(), 1);
+
+            for z in &mut self.zombies {
+                z.render(a);
+                // TODO check if zombie is list and visible
+            }
+
+            // TODO render with particle engine
+            gl::Disable(2896);
             gl::Disable(gl::TEXTURE_2D);
+            gl::Disable(2912);
 
             if let Some(ref hit) = self.hit_result {
+                gl::Disable(3008);
                 self.level_renderer
                     .as_mut()
                     .unwrap()
                     .borrow_mut()
                     .render_hit(hit);
+                gl::Enable(3008);
             }
 
-            // for some reason, in the original code, there's a `new Cube(0, 0)` here
-            gl::Disable(gl::FOG);
+            // TODO draw gui
             update_display();
         }
+    }
+
+    // TODO drawGui
+
+    fn setup_fog(&mut self, i: JInt) {
+        unsafe {
+            if i == 0 {
+                gl::Fogi(2917, 2048);
+                gl::Fogf(2914, 0.001);
+                gl::Fogfv(2918, self.fog_color_0.as_mut_ptr());
+                gl::Disable(2896);
+            } else if i == 1 {
+                gl::Fogi(2917, 2048);
+                gl::Fogf(2914, 0.06);
+                gl::Fogfv(2918, self.fog_color_1.as_mut_ptr());
+                gl::Enable(2896);
+                gl::Enable(2903);
+                let br = 0.6;
+                gl::LightModelfv(2899, self.get_buffer(br, br, br, 1.0).as_mut_ptr());
+            }
+        }
+    }
+
+    fn get_buffer(&self, a: JFloat, b: JFloat, c: JFloat, d: JFloat) -> Vec<JFloat> {
+        vec![a, b, c, d]
     }
 }
 
